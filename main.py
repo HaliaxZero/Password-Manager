a2e735dc8c77b03f3c101d74d2c5ea77de2d600d
Brian Reichel <Reichel.Brian@t-online.de>
2025-09-26 21:22:01 +0200

    added basic functionality to the password manager

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
 from tkinter import *
+from tkinter import messagebox
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8,10))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2,4))]
+
+    password_list = password_numbers + password_letters + password_symbols
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website= website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0 or len(email) == 0:
+        messagebox.showinfo(title="Empty Fields!", message="Please make sure to fill out all Fields")
+    else:
+
+        is_ok = messagebox.askokcancel(title=website, message=f"Therese are your Details: \n Email: {email} "
+                                                              f"\n Password:{password} \n Is it okay to save?")
+
+
+        if is_ok:
+            with open("data.txt","a") as data_file:
+                data_file.write(f"{website}|{email}|{password}\n")
+                website_entry.delete(0, END)
+                email_entry.delete(0, END)
+                password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -35,10 +73,11 @@ password_label.grid(column=0,row=4)
 password_entry = Entry(width=21)
 password_entry.grid(row = 4, column= 1, columnspan=1)
 
-password_button = Button(width=17, text="Generate Password")
+
+password_button = Button(width=17, text="Generate Password", command=generate_password)
 password_button.grid(row=4, column= 2)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command= save)
 add_button.grid(row=5,column=1, columnspan=2)
 
 
